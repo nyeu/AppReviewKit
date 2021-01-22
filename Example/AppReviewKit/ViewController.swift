4e719f2ee8d486ca786fcf60fd6598c955034c59
Joan <joancardona.17@gmail.com>
2021-01-22 16:03:44 +0100

    Add how to use it and a small example project

--- Example/AppReviewKit/ViewController.swift
+++ Example/AppReviewKit/ViewController.swift
@@ -7,18 +7,40 @@
 //
 
 import UIKit
+import AppReviewKit
 
 class ViewController: UIViewController {
-
+    let button: UIButton = {
+        let b = UIButton()
+        b.setTitle("Touch To Ask for Review", for: .normal)
+        b.setTitleColor(.blue, for: .normal)
+        return b
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(button)
+        
+        button.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+            make.height.equalTo(40)
+            make.width.equalToSuperview().offset(-50)
+        }
+        
+        button.addTarget(self, action: #selector(tapped), for: .touchUpInside)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func tapped() {
+        openReviewKit()
     }
-
+    
+    func openReviewKit() {
+        let styleConfiguration = AppReviewStyleConfiguration(backgroundColor: .white,
+                                                             titleColor: .black,
+                                                             descriptionColor: .gray)
+        
+        let reviewKit = AppReviewViewController(reviewTitle: "Do you like Mindful Affirmations?", reviewDescription: "It would help us a lot knowing your opinion", styleConfiguration: nil)
+        present(reviewKit, animated: true, completion: nil)
+    }
 }
 
